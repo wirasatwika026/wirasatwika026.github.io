5a70edadc1a5d7bf98991dc42ad1376f3d999306
wirasatwika026 <wiraadrai@gmail.com>
2025-07-11 18:17:25 +0000

    Enhance navbar functionality and improve CSS animations for better user experience

--- js/script.js
+++ js/script.js
@@ -34,7 +34,8 @@ window.onscroll = () => {
 
 
     // remove toggle icon and navbar when click navbar links (scroll)
-
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
     // animation footer on scroll
 
